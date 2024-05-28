2c486ae7ea92f769f01d5adb594e7655ad8fd2d9
Wolfcharm <ichen.kos94@gmail.com>
2024-05-28 04:07:39 +0300

    changes

--- UI.py
+++ UI.py
@@ -7,24 +7,53 @@ import qdarktheme
 import Opener
 
 path = "C:/Program Files/Marmoset/Toolbag 4/toolbag.exe"
-pyfile = "C:/Users/ichen/Documents/Python Projects/textureConvert/main.py"
+pyfile = "main.py"
 allowedImageFormats = ['.jpg', '.png', '.tga', '.psd', '.psb', '.exr', '.hdr', '.mpic', '.bmp', '.dds', '.tig', '.pfm']
-
-
-class ImageChannels(QLayout):
-    def __init__(self, enableR, enableG, enableB, enableA, parent):
+savePath = ''
+class ImageChannel(QHBoxLayout):
+    def __init__(self, labelName: str, selectionItems: list, parent):
         super().__init__(parent)
 
-        self.label = QLabel(parent)
-
-        self.selectionR = QComboBox(parent)
-        self.selectionR.setFixedSize(20, 10)
+        self.label = QLabel(labelName, parent)
+        self.selection = QComboBox(parent)
+        self.selection.setFixedSize(50, 10)
+        self.selection.addItems(selectionItems)
 
         self.addWidget(self.label)
-        self.addWidget(self.selectionR)
+        self.addWidget(self.selection)
+
+class ImageChannels(QVBoxLayout):
+    def __init__(self, enableR: bool, enableG: bool, enableB: bool, enableA: bool, parent):
+        super().__init__(parent)
+        widget = QWidget(parent)
+
+        selectionItems = ['R', 'G', 'B', 'A', '1', '0', '1-R', '1-G', '1-B', '1-A']
+        if enableR:
+            R = ImageChannel('R', selectionItems, parent)
+            self.addLayout(R)
+        if enableG:
+            G = ImageChannel('G', selectionItems, parent)
+            self.addLayout(G)
+        if enableB:
+            B = ImageChannel('B', selectionItems, parent)
+            self.addLayout(B)
+        if enableA:
+            A = ImageChannel('A', selectionItems, parent)
+            self.addLayout(A)
+
+
+class ImageChannelsGrayscale(QVBoxLayout):
+    def __init__(self, enableGrayscale: bool, parent):
+        super().__init__(parent)
+        widget = QWidget(parent)
+        selectionItems = ['R', 'G', 'B', 'A', '1', '0', '1-R', '1-G', '1-B', '1-A']
+        if enableGrayscale:
+            Gr = ImageChannel('Grayscale', selectionItems, parent)
+            self.addLayout(Gr)
+
 
 class DropArea(QLabel):
-    def __init__(self, title, sizex, sizey, parent):
+    def __init__(self, title: str, sizex: int, sizey: int, parent):
         super().__init__(parent)
 
         self.setAcceptDrops(True)
@@ -32,13 +61,15 @@ class DropArea(QLabel):
         self._defaultText = title
         self.filePath = ''
         self.setText(self._defaultText)
+        self.sizex = sizex
+        self.sizey = sizey
         self.setFixedSize(sizex, sizey)
         self.setStyleSheet('border: 2px dashed #aaa')
         self.mask = QBitmap('mask.png')
 
 
     def setPixmap(self, image):
-        super().setPixmap(image)
+        super().setPixmap(image.scaled(self.sizex,self.sizey,Qt.KeepAspectRatio, 1))
         super().setStyleSheet('')
         super().setMask(self.mask)
         super().setStyleSheet('box-shadow: 3px')
@@ -77,6 +108,29 @@ class DropArea(QLabel):
         if e.button() == Qt.RightButton:
             self.clearPixmap()
 
+class TextureCard(QHBoxLayout):
+    def __init__(self, title: str, previewsizex: int, previewsizey: int, displaychannelR: bool, displaychannelG: bool, displaychannelB: bool, displaychannelA: bool, parent):
+        super().__init__(parent)
+
+        self.textureCardHLayout = QHBoxLayout(parent)
+        self.dropAreaAlbedo = DropArea(title, previewsizex, previewsizey, parent)
+        self.textureCardHLayout.addWidget(self.dropAreaAlbedo)
+        self.textureChannelsVLayout = ImageChannels(displaychannelR, displaychannelG, displaychannelB, displaychannelA, parent)
+
+        self.textureCardHLayout.addLayout(self.textureChannelsVLayout)
+        self.addLayout(self.textureCardHLayout)
+
+class TextureCardGrayscale(QHBoxLayout):
+
+    def __init__(self, title: str, previewsizex: int, previewsizey: int, displayGrayscale: bool, parent):
+        super().__init__(parent)
+        self.textureCardHLayout = QHBoxLayout(parent)
+        self.dropAreaAlbedo = DropArea(title, previewsizex, previewsizey, parent)
+        self.textureCardHLayout.addWidget(self.dropAreaAlbedo)
+        self.textureChannelsVLayout = ImageChannelsGrayscale(displayGrayscale, parent)
+
+        self.textureCardHLayout.addLayout(self.textureChannelsVLayout)
+        self.addLayout(self.textureCardHLayout)
 
 class MainUI(QMainWindow):
 
@@ -86,92 +140,33 @@ class MainUI(QMainWindow):
         mainWindow = QWidget(self)
         mainVLayout = QVBoxLayout(self)
 
-        self.setGeometry(300, 300, 500, 200)
+        savePathHlayout = QHBoxLayout(self)
+        self.pathField = QLineEdit(self)
+        self.pathField.resize(350, 20)
+        self.pathField.setText(savePath)
+        self.pathField.textChanged.connect(self.updateSavePath)
+        btn_browseSavePath = QPushButton('Browse...', self)
+        btn_browseSavePath.clicked.connect(self.selectSavePath)
+        savePathHlayout.addWidget(self.pathField)
+        savePathHlayout.addWidget(btn_browseSavePath)
 
 
-        textureCardHLayout = QHBoxLayout(self)
-        dropAreaAlbedo = DropArea('Albedo', 100, 100, self)
-        textureCardHLayout.addWidget(dropAreaAlbedo)
-        textureChannelsVLayout = QVBoxLayout(self)
-        channelHLayoutR = QHBoxLayout(self)
-        R = QLabel('R', self)
-        channelHLayoutR.addWidget(R)
-        RComboBox = QComboBox(self)
-        channelHLayoutR.addWidget(RComboBox)
-        textureChannelsVLayout.addLayout(channelHLayoutR)
-        textureCardHLayout.addLayout(textureChannelsVLayout)
+        textureCardAlb = TextureCard('Albedo', 150, 150, False, False, False, False, self)
+        textureCardMet = TextureCardGrayscale('Metal', 150, 150, True, self)
+        textureCardRough = TextureCardGrayscale('Roughness', 150, 150, True, self)
 
-        mainVLayout.addLayout(textureCardHLayout)
-        spacerItem = QSpacerItem(378, 13, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        mainVLayout.addLayout(textureCardAlb)
+        mainVLayout.addLayout(textureCardMet)
+        mainVLayout.addLayout(textureCardRough)
+        mainVLayout.addLayout(savePathHlayout)
+        mainVLayout.addStretch(1)
         buttonRun = QPushButton('Run', self)
+        buttonRun.clicked.connect(self.runProcess)
         mainVLayout.addWidget(buttonRun)
-        mainVLayout.addItem(spacerItem)
 
         mainWindow.setLayout(mainVLayout)
         self.setCentralWidget(mainWindow)
-
-
-
-        #
-        # btn_browseExe = QPushButton('Browse...', self)
-        # btn_browseExe.resize(btn_browseExe.sizeHint())
-        # btn_browseExe.clicked.connect(self.openFileDialog)
-        #
-        # global pathField
-        # pathField = QLineEdit(self)
-        # # pathField.resize(250, 20)
-        # pathField.setText(path)
-        # pathField.textChanged.connect(self.updatePath)
-        #
-        # btn_Run = QPushButton('Run', self)
-        # btn_Run.resize(btn_browseExe.sizeHint())
-        # btn_Run.clicked.connect(self.runProcess)
-        #
-        # drop1 = DropArea('Albedo', 100, 100, self)
-        # drop2 = DropArea('Metal', 100, 100, self)
-        # drop3 = DropArea('Gloss', 100, 100, self)
-        # channelsSelector1 = ImageChannels(True, True, True,True, self)
-        # channelsSelector2 = ImageChannels(True, True, True,True, self)
-        # channelsSelector3 = ImageChannels(True, True, True,True, self)
-        #
-        # vertTexturePanel1 = QVBoxLayout(self.parent())
-        # vertTexturePanel1.addWidget(drop1)
-        # vertTexturePanel1.addWidget(channelsSelector1)
-        #
-        # vertTexturePanel2 = QVBoxLayout(self.parent())
-        # vertTexturePanel2.addWidget(drop2)
-        # vertTexturePanel2.addWidget(channelsSelector2)
-        #
-        # vertTexturePanel3 = QVBoxLayout(self.parent())
-        # vertTexturePanel3.addWidget(drop3)
-        # vertTexturePanel3.addWidget(channelsSelector3)
-        #
-        # texturesBox_layout = QHBoxLayout(self.parent())
-        # texturesBox_layout.addLayout(vertTexturePanel1)
-        # texturesBox_layout.addLayout(vertTexturePanel2)
-        # texturesBox_layout.addLayout(vertTexturePanel3)
-        # texturesBox_layout.addWidget(drop2)
-        # texturesBox_layout.addWidget(drop3)
-        #
-        # selectExeHBox = QHBoxLayout()
-        # selectExeHBox.setParent(mainLayout)
-        # selectExeHBox.addWidget(pathField)
-        # selectExeHBox.addWidget(btn_browseExe)
-        #
-        # vbox = QVBoxLayout()
-        # vbox.addLayout(selectExeHBox)
-        # vbox.addLayout(texturesBox_layout)
-        # vbox.addStretch(1)
-        # vbox.addWidget(btn_Run)
-        #
-        # mainLayout.addLayout(selectExeHBox)
-        # mainLayout.addStretch()
-        #
-        # widget = QWidget()
-        # widget.setLayout(selectExeHBox)
-        # self.setCentralWidget(widget)
-
-        #self.setGeometry(300, 300, 500, 200)
+        self.setGeometry(300, 300, 400, 0)
         self.setWindowTitle('Tooltips')
         self.show()
 
@@ -180,11 +175,11 @@ class MainUI(QMainWindow):
     #     pathField.setText(self.selectExe())
     #     self.updatePath()
     #
-    # @pyqtSlot(name='runprocess')
-    # def runProcess(self):
-    #     Opener.open_(path, pyfile)
-    #     print(path)
-    #
+    @pyqtSlot(name='runprocess')
+    def runProcess(self):
+        Opener.open_(path, pyfile)
+        print(path)
+
     # def updatePath(self):
     #     global path
     #     path = pathField.text()
@@ -196,7 +191,15 @@ class MainUI(QMainWindow):
     #                                               "Executable (*.exe)", options=fileopen)
     #     if fileName:
     #         return fileName
-
+    def selectSavePath(self):
+        savePath_ = str(QFileDialog.getExistingDirectory(self, "Select Save Directory"))
+        if savePath_:
+            self.pathField.setText(savePath_)
+            return savePath_
+    def updateSavePath(self):
+            global savePath
+            path = self.pathField.text()
+            print(path)
 
 if __name__ == '__main__':
     qdarktheme.enable_hi_dpi()

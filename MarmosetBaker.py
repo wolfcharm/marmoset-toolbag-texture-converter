707c66b2c9da9910a4703704a4ba8785ba3531e6
Wolfcharm <ichen.kos94@gmail.com>
2024-06-04 03:58:13 +0300

    changes

--- MarmosetBaker.py
+++ MarmosetBaker.py
@@ -1,18 +1,25 @@
 import mset
 import sys
 
-sys.argv
-print('AAAAAAAAAAAAAAAAAAAAAAAAAAAA', sys.argv)
+print('Marmoset Texture Converter run parameters', sys.argv)
 
-albedoTexturePath = ''
-metallicTexturePath = ''
-roughnessTexturePath = ''
+albedoTexturePath = sys.argv[1]
+metallicTexturePath = sys.argv[2]
+roughnessTexturePath = sys.argv[3]
 
-albedoMap = mset.MaterialSurfaceMap
-albedoMap.texture = albedoTexturePath
-
-baker = mset.BakerObject
+baker = mset.BakerObject()
 baker.name = 'Marmoset Baker Executable'
-hiMat = mset.Material
-hiMat.name = 'High'
-hiMat.albedo = albedoMap
+
+hiMat = mset.Material('High')
+hiMat.albedo.setField('Albedo Map', albedoTexturePath)
+hiMat.albedo.getField("Albedo Map").sRGB = True
+
+#['Metalness Map', 'Channel', 'Metalness', 'Invert']
+hiMat.reflectivity.setField('Metalness Map', metallicTexturePath)
+hiMat.reflectivity.getField("Metalness Map").sRGB = False
+
+#['Roughness Map', 'Channel', 'Roughness', 'Invert;roughness']
+hiMat.microsurface.setField('Roughness Map', metallicTexturePath)
+hiMat.microsurface.getField("Roughness Map").sRGB = False
+
+

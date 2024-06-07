14f134c9d53357d5c63d669631544a7cf5b224b0
Wolfcharm <ichen.kos94@gmail.com>
2024-06-07 15:05:08 +0300

    changes

--- MarmosetBaker.py
+++ MarmosetBaker.py
@@ -42,6 +42,21 @@ class Recipe(object):
         self.bakerMesh = file.readline().rstrip('\n')
         file.close()
 
+def interpret_channels(index: int) -> [int, bool]:
+    if index <= 3:
+        return index, False
+    if index == 4:  # white
+        pass
+    if index == 5:  # black
+        pass
+    if index == 6:  # 1-R
+        return 0, True
+    if index == 7:  # 1-G
+        return 1, True
+    if index == 8:  # 1-B
+        return 2, True
+    if index == 9:  # 1-A
+        return 3, True
 
 recipe = Recipe()
 
@@ -53,19 +68,23 @@ hiMat.albedo.setField('Albedo Map', recipe.albedoTexturePath)
 hiMat.albedo.getField("Albedo Map").sRGB = True
 
 #['Metalness Map', 'Channel', 'Metalness', 'Invert']
+hiMat.reflectivity.setField('Metalness', 1.0)
 hiMat.reflectivity.setField('Metalness Map', recipe.metallicTexturePath)
-hiMat.reflectivity.setField('Channel', int(recipe.metallicChannel))
+chIndex, invert = interpret_channels(int(recipe.metallicChannel))
+hiMat.reflectivity.setField('Channel', chIndex)
+hiMat.reflectivity.setField('Invert', invert)
 hiMat.reflectivity.getField("Metalness Map").sRGB = False
-
 #['Roughness Map', 'Channel', 'Roughness', 'Invert;roughness']
+hiMat.microsurface.setField('Roughness', 1.0)
 hiMat.microsurface.setField('Roughness Map', recipe.metallicTexturePath)
-hiMat.microsurface.setField('Channel', int(recipe.roughnessChannel))
+chIndex, invert = interpret_channels(int(recipe.roughnessChannel))
+hiMat.microsurface.setField('Channel', chIndex)
+hiMat.microsurface.setField('Invert;roughness', invert)
 hiMat.microsurface.getField("Roughness Map").sRGB = False
 hiMat.microsurface.getFieldNames()
 
 modelHi = mset.importModel(recipe.bakerMesh)
 modelHi.name = 'Quad_hi'
-
 modelLow = modelHi.duplicate('Quad_low')
 
 baker.outputSamples = int(recipe.samples)
@@ -85,7 +104,7 @@ modelLow.parent = bakerLow
 hiMat.assign(modelHi, True)
 
 mset.bakeAll()
-mset.quit()
+#mset.quit()
 
 #print(recipe.albedoTexturePath, recipe.metallicTexturePath, recipe.metallicChannel, recipe.roughnessTexturePath,
 #      recipe.roughnessChannel, recipe.bakerMesh)
